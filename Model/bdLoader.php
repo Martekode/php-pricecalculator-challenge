f4fd7bf3f27d51ad5d6961e408e38b22b43929f1
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-07-08 10:51:22 +0200

    Add dbLoader file

---
+++ Model/bdLoader.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+
+class DBLoader {
+    private string $dbname;
+    private string $username;
+    private string $password;
+    private string $servername;
+    private PDO $conn;
+
+    public function __construct() {
+        $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,1),".env");
+        $dotenv->load();
+        $this->dbname = $_ENV['DB_NAME'];
+        $this->username = $_ENV['USER_NAME'];
+        $this->password = $_ENV['USER_PASSWORD'];
+        $this->servername = getenv('SERVER_NAME');
+        $this->makeConnection();
+    }
+    public function makeConnection() {
+        try{
+            $this->conn = new PDO("mysql:dbname=". $this->dbname.";host=".$this->servername,$this->username,$this->password);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+    public function getConn() {
+        return $this->conn;
+    }
+}
+
+
+
--- code.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once ('vendor/autoload.php');
-$username = getenv('USER_NAME');
-$password = getenv('USER_PASSWORD');
-$servername = getenv('SERVER_NAME');
-
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,1),"./php-pricecalculator-challenge/.env");
-$dotenv->load();
