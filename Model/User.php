377949390add342525aa77effdf8565083639832
Martekode <BrianDesmedt9000@hotmail.com>
2022-07-12 14:18:26 +0200

    created multiple contructors in users

--- Model/User.php
+++ Model/User.php
@@ -1,16 +1,32 @@
 <?php
+
 declare(strict_types=1);
 
 class User
 {
     private $name;
+    private string $password;
 
-    public function __construct(string $name)
+    public function __construct()
+    {
+        $arguments = func_get_args();
+        $numberOfArguments = func_num_args();
+
+        if (method_exists($this, $function = '__construct' . $numberOfArguments)) {
+            call_user_func_array(array($this, $function), $arguments);
+        }
+    }
+    public function __construct1($name)
     {
         $this->name = $name;
     }
+    public function __construct2($name, $password)
+    {
+        $this->name = $name;
+        $this->password = $password;
+    }
 
-    public function getName() : string
+    public function getName(): string
     {
         return $this->name;
     }
